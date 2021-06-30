c8e237e97b846b2f8bb52264d6fd5f6b57ae9922
okomogo1 <okomogo1@gmail.com>
2021-06-30 17:49:41 +0100

    Whatsapp Tab

--- app/src/main/java/com/timothy/tabs/MainActivity.kt
+++ app/src/main/java/com/timothy/tabs/MainActivity.kt
@@ -8,9 +8,13 @@ import com.timothy.tabs.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var whatsAppAdapter: WhatsAppAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        whatsAppAdapter = WhatsAppAdapter(supportFragmentManager)
+        binding.pager.adapter = whatsAppAdapter
+        binding.tabLayout.setupWithViewPager(binding.pager)
     }
 }
